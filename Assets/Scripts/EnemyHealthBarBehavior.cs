4769b97c39fb4d6bffc318d80eb083b96e52a62d
kaiaslind <kaiaslind@gmail.com>
2025-07-08 16:13:59 -0400

    implemented enemy health bar

---
+++ Assets/Scripts/EnemyHealthBarBehavior.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthBarBehavior : MonoBehaviour
+{
+    public GameObject BattleController;
+
+    private float maxHP;
+    private float HP;
+    private float scale;
+
+    public GameObject insideBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update HP and maxHP with values from BattleControl
+        maxHP = BattleController.GetComponent<BattleControl>().enemyHPmax;
+        HP = BattleController.GetComponent<BattleControl>().enemyHP;
+
+        //resize the HP bar
+        scale = HP / maxHP;
+        insideBar.transform.localScale = new Vector3(scale, 1, 1);
+    }
+}
