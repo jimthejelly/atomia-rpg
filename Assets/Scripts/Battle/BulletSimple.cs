d5a923c076d445c4be2cf0113b6e2dca2fb6c3d5
kaiaslind <kaiaslind@gmail.com>
2025-07-25 17:57:23 -0400

    implemented sample bullet pattern

---
+++ Assets/Scripts/Battle/BulletSimple.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSimple : MonoBehaviour
+{
+    public int dir; //0 up, 1 right, 2 down, 3 left
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dir == 0)
+        {
+            transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
+        }
+        else if (dir == 1)
+        {
+            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+        }
+        if (dir == 2)
+        {
+            transform.position += new Vector3(0, -1*speed, 0) * Time.deltaTime;
+        }
+        else if (dir == 3)
+        {
+            transform.position += new Vector3(-1*speed, 0, 0) * Time.deltaTime;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Text: colliding!");
+    }
+}
---
+++ Assets/Scripts/Battle/BulletsTest.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletsTest : MonoBehaviour
+{
+    public GameObject[] bullettypes;
+    double spawntimer = 0;
+    double roundtimer = 0;
+    int pattern = 0;
+    bool inprogress = false;
+    public List<GameObject> activebullets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inprogress = true;
+        activebullets = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inprogress)
+        {
+            spawntimer += Time.deltaTime;
+            roundtimer += Time.deltaTime;
+
+            //bullet pattern #0
+            if (pattern == 0)
+            {
+                //generates a new bullet after a certain amount of time
+                if (spawntimer > 0.25)
+                {
+                    int dir = Random.Range(0, 4);
+                    float xypos = Random.Range(-1.4f, 1.4f);
+                    float x, y;
+
+                    if (dir == 0) //up. bullets generate at xypos x, -1.5 y
+                    {
+                        x = xypos;
+                        y = -2f;
+                    }
+                    else if (dir == 1) //right. bullets generate at -1.5 x, xypos y
+                    {
+                        x = -2f;
+                        y = xypos;
+                    }
+                    else if (dir == 2) //down. bullets generate at xypos x, 1.5 y
+                    {
+                        x = xypos;
+                        y = 2f;
+                    }
+                    else//left. bullets generate at 1.5 x, xypos y
+                    {
+                        x = 2f;
+                        y = xypos;
+                    }
+                    GenerateBullet(0, x, y);
+
+                    //change direction of generated bullet
+                    activebullets[activebullets.Count - 1].GetComponent<BulletSimple>().dir = dir;
+
+                    //reset spawntimer
+                    spawntimer = 0;
+                }
+                //exits the current pattern after a certain amount of time
+                if (roundtimer > 10)
+                {
+                    inprogress = false;
+                }
+            }
+        }
+    }
+
+    void GenerateBullet(int id, float x, float y) //xypos is the position in either the x or y direction, as determined by dir (direction)
+    {
+        //error prevention
+        if (id < 0 || id > bullettypes.Length) { return; }
+
+        Vector3 pos = new Vector3(x, y, 0);
+        Quaternion rot = new Quaternion(0, 0, 0, 0);
+        GameObject temp = Instantiate(bullettypes[id],pos,rot);
+        activebullets.Add(temp);
+    }
+}
