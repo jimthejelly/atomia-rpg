8c4e710a70f8fccfed8dd8e52bf27122b8033368
Sohan Biswal <101779634+sohan-4@users.noreply.github.com>
2025-10-24 17:21:00 -0400

    Fully setting up and fixing the volume slider

--- Assets/Scripts/Main Menu/VolumeSliderScript.cs
+++ Assets/Scripts/Main Menu/VolumeSliderScript.cs
@@ -9,13 +9,18 @@ using UnityEngine.UI;
 
 [RequireComponent(typeof(Slider))]
 [RequireComponent(typeof(AudioSource))]
-public class VolumeSliderScript : MonoBehaviour, IPointerUpHandler
+public class VolumeSliderScript : MonoBehaviour, IPointerUpHandler, IMoveHandler
 {
+
+    int counter = 0;
 
     private AudioSource audioData;
 
     private Slider volumeSlider;
     private float volumeLevel;
+
+    public float stepSize = 0.01f;
+    public float defaultStepSize = 0.1f;
 
     public TMPro.TextMeshProUGUI volumeLabel;
 
@@ -33,6 +38,53 @@ public class VolumeSliderScript : MonoBehaviour, IPointerUpHandler
     // Update is called once per frame
     void Update()
     {
+        if (counter > 0)
+        {
+            counter--;
+        }
+
+        if (EventSystem.current.currentSelectedGameObject == volumeSlider.gameObject)
+        {
+            CheckForVolumeKeys();
+        }
+    }
+
+    private void CheckForVolumeKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0)){
+            volumeSlider.value = 0.0f;
+            OnPointerUp(null);
+        }else if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)){
+            volumeSlider.value = 0.1f;
+            OnPointerUp(null);
+        }else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)){
+            volumeSlider.value = 0.2f;
+            OnPointerUp(null);
+        }else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3)){
+            volumeSlider.value = 0.3f;
+            OnPointerUp(null);
+        }else if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4)){
+            volumeSlider.value = 0.4f;
+            OnPointerUp(null);
+        }else if (Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Keypad5)){
+            volumeSlider.value = 0.5f;
+            OnPointerUp(null);
+        }else if (Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.Keypad6)){
+            volumeSlider.value = 0.6f;
+            OnPointerUp(null);
+        }else if (Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Keypad7)){
+            volumeSlider.value = 0.7f;
+            OnPointerUp(null);
+        }else if (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8)){
+            volumeSlider.value = 0.8f;
+            OnPointerUp(null);
+        }else if (Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9)){
+            volumeSlider.value = 0.9f;
+            OnPointerUp(null);
+        }else if (Input.GetKeyDown(KeyCode.F)){
+            volumeSlider.value = 1.0f;
+            OnPointerUp(null);
+        }
     }
 
     private void UpdateVolume(float value)
@@ -46,4 +98,31 @@ public class VolumeSliderScript : MonoBehaviour, IPointerUpHandler
     {
         audioData.Play();
     }
+
+    public void OnMove(AxisEventData eventData)
+    {
+        if (eventData.moveDir == MoveDirection.Left)
+        {
+            volumeSlider.value -= stepSize;
+            volumeSlider.value += defaultStepSize;
+            if (counter == 0)
+            {
+                counter = 40;
+                OnPointerUp(null);
+            }
+        }
+        else if (eventData.moveDir == MoveDirection.Right)
+        {
+            volumeSlider.value += stepSize;
+            volumeSlider.value -= defaultStepSize;
+            if (counter == 0)
+            {
+                counter = 40;
+                OnPointerUp(null);
+            }
+        }
+    }
+
+
+
 }
