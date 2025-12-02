281948db611bb93eb0d44cc62249f5dd5b8aee88
Sohan Biswal <101779634+sohan-4@users.noreply.github.com>
2025-12-02 13:13:58 -0500

    everything

--- Assets/Scripts/Main Menu/KeyboardNavigationScript.cs
+++ Assets/Scripts/Main Menu/KeyboardNavigationScript.cs
@@ -3,90 +3,151 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class KeyboardNavigationScript : MonoBehaviour
 {
     public Slider volumeSlider;
+    public TMP_Dropdown qualityDropdown;
     public Button saveButton;
+    public Button loadButton;
+
     private Outline sliderOutline;
-    private Outline buttonOutline;
+    private Outline dropdownOutline;
+    private Outline saveOutline;
+    private Outline loadOutline;
+
     private GameObject sliderGO;
-    private GameObject buttonGO;
+    private GameObject dropdownGO;
+    private GameObject saveGO;
+    private GameObject loadGO;
 
     void Awake()
     {
         SetupNavigation();
         SetupOutlines();
     }
+
     void OnEnable()
     {
         EventSystem.current.SetSelectedGameObject(null);
         EventSystem.current.SetSelectedGameObject(sliderGO);
     }
-    
+
     void OnDisable()
     {
         if (sliderOutline != null) sliderOutline.enabled = false;
-        if (buttonOutline != null) buttonOutline.enabled = false;
+        if (dropdownOutline != null) dropdownOutline.enabled = false;
+        if (saveOutline != null) saveOutline.enabled = false;
+        if (loadOutline != null) loadOutline.enabled = false;
     }
 
     void Update()
     {
         GameObject currentSelected = EventSystem.current.currentSelectedGameObject;
 
-        if (currentSelected == sliderGO){
-            sliderOutline.enabled = true;
-            buttonOutline.enabled = false;
-        }else if (currentSelected == buttonGO){
-            sliderOutline.enabled = false;
-            buttonOutline.enabled = true;
-        }else{
-            sliderOutline.enabled = false;
-            buttonOutline.enabled = false;
+        if (currentSelected == sliderGO)
+        {
+            EnableOutline(sliderOutline);
+        }
+        else if (currentSelected == dropdownGO)
+        {
+            EnableOutline(dropdownOutline);
+        }
+        else if (currentSelected == saveGO)
+        {
+            EnableOutline(saveOutline);
+        }
+        else if (currentSelected == loadGO)
+        {
+            EnableOutline(loadOutline);
+        }
+        else
+        {
+            DisableAllOutlines();
         }
     }
 
     void SetupNavigation()
     {
-
+        // Volume slider navigation
         Navigation navSlider = volumeSlider.navigation;
         navSlider.mode = Navigation.Mode.Explicit;
-        navSlider.selectOnDown = saveButton;
+        navSlider.selectOnDown = qualityDropdown;
         volumeSlider.navigation = navSlider;
 
-        Navigation navButton = saveButton.navigation;
-        navButton.mode = Navigation.Mode.Explicit;
-        navButton.selectOnUp = volumeSlider;
-        saveButton.navigation = navButton;
+        // Quality dropdown navigation
+        Navigation navDropdown = qualityDropdown.navigation;
+        navDropdown.mode = Navigation.Mode.Explicit;
+        navDropdown.selectOnUp = volumeSlider;
+        navDropdown.selectOnDown = saveButton;
+        qualityDropdown.navigation = navDropdown;
+
+        // Save button navigation
+        Navigation navSave = saveButton.navigation;
+        navSave.mode = Navigation.Mode.Explicit;
+        navSave.selectOnUp = qualityDropdown;
+        navSave.selectOnDown = loadButton;
+        saveButton.navigation = navSave;
+
+        // Load button navigation
+        Navigation navLoad = loadButton.navigation;
+        navLoad.mode = Navigation.Mode.Explicit;
+        navLoad.selectOnUp = saveButton;
+        loadButton.navigation = navLoad;
     }
 
     void SetupOutlines()
     {
         sliderGO = volumeSlider.gameObject;
-        buttonGO = saveButton.gameObject;
+        dropdownGO = qualityDropdown.gameObject;
+        saveGO = saveButton.gameObject;
+        loadGO = loadButton.gameObject;
 
-        sliderOutline = sliderGO.GetComponent<Outline>();
-        if (sliderOutline == null)
-        {
-            sliderOutline = sliderGO.AddComponent<Outline>();
-        }
-
-        buttonOutline = buttonGO.GetComponent<Outline>();
-        if (buttonOutline == null)
-        {
-            buttonOutline = buttonGO.AddComponent<Outline>();
-        }
+        sliderOutline = GetOrAddOutline(sliderGO);
+        dropdownOutline = GetOrAddOutline(dropdownGO);
+        saveOutline = GetOrAddOutline(saveGO);
+        loadOutline = GetOrAddOutline(loadGO);
 
         Color redColor = Color.red;
         Vector2 outlineDistance = new Vector2(2f, -2f);
 
         sliderOutline.effectColor = redColor;
-        sliderOutline.effectDistance = outlineDistance;
-        
-        buttonOutline.effectColor = redColor;
-        buttonOutline.effectDistance = outlineDistance;
+        dropdownOutline.effectColor = redColor;
+        saveOutline.effectColor = redColor;
+        loadOutline.effectColor = redColor;
 
+        sliderOutline.effectDistance = outlineDistance;
+        dropdownOutline.effectDistance = outlineDistance;
+        saveOutline.effectDistance = outlineDistance;
+        loadOutline.effectDistance = outlineDistance;
+
+        DisableAllOutlines();
+    }
+
+    Outline GetOrAddOutline(GameObject go)
+    {
+        Outline outline = go.GetComponent<Outline>();
+        if (outline == null)
+        {
+            outline = go.AddComponent<Outline>();
+        }
+        return outline;
+    }
+
+    void EnableOutline(Outline active)
+    {
+        sliderOutline.enabled = (active == sliderOutline);
+        dropdownOutline.enabled = (active == dropdownOutline);
+        saveOutline.enabled = (active == saveOutline);
+        loadOutline.enabled = (active == loadOutline);
+    }
+
+    void DisableAllOutlines()
+    {
         sliderOutline.enabled = false;
-        buttonOutline.enabled = false;
+        dropdownOutline.enabled = false;
+        saveOutline.enabled = false;
+        loadOutline.enabled = false;
     }
 }
